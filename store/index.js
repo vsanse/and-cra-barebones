e764a949ddf6eb67f73ab21db5560291bd68a420
Vishal Sanserwal <vsanse@outlook.com>
2020-03-10 20:06:21 +0530

    store updated

--- store/index.js
+++ store/index.js
@@ -1,8 +1,18 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
 import { rootReducer } from "./rootReducer";
+
+// Allow redux devtools only for development build 
+const devTools =
+  process.env.NODE_ENV === "production"
+    ? applyMiddleware(thunk)
+    : composeWithDevTools(applyMiddleware(thunk));
 
 export function configureStore() {
     return createStore(
-        rootReducer
+        rootReducer,
+        devTools
     );
 }
